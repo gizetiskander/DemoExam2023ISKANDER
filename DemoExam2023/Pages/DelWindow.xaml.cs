c8233058e3641b8220062d3c41c520d83f747341
Гизетдинов Искандер <gizetiskander@gmail.com>
2023-03-04 13:10:44 +0300

    delwindow done

--- DemoExam2023/Pages/DelWindow.xaml.cs
+++ DemoExam2023/Pages/DelWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoExam2023.db;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +15,45 @@ using System.Windows.Shapes;
 
 namespace DemoExam2023.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для DelWindow.xaml
-    /// </summary>
+    
     public partial class DelWindow : Window
     {
+        public static ToiletPaperShop_dbEntities dBEntities = new ToiletPaperShop_dbEntities();
         public DelWindow()
         {
             InitializeComponent();
+            AgentLst.ItemsSource = dBEntities.Product.ToList();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AgentListWindow agentWindow = new AgentListWindow();
+            agentWindow.Show();
+            this.Close();
+        }
+
+        private void DelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var q = AgentLst.SelectedItem as Product;
+            if (q == null)
+            {
+                MessageBox.Show("Ничего не выбрано!");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите удалить строку?", "Удалить?", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dBEntities.Product.Remove(q);
+                    AgentLst.ItemsSource = dBEntities.Product.ToList();
+                    dBEntities.SaveChanges();
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка!");
+                }
+            }
         }
     }
 }
